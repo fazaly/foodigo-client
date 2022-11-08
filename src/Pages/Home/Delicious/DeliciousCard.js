01f38636ebc628af645760221e7c3b07edabac01
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-08 18:24:35 +0600

    delicious section added

--- src/Pages/Home/Delicious/DeliciousCard.js
+++ src/Pages/Home/Delicious/DeliciousCard.js
@@ -4,7 +4,15 @@ const DeliciousCard = ({delicious}) => {
     const {id, img, title, details} = delicious;
     return (
         <div className='card card-compact sm:w-96 sm:h-full bg-base-200 shadow-xl'>
-            
+            <div>
+                <img src={img} alt="" className="w-full rounded-lg p-4 mb-2 h-60 sm:h-96" />
+            </div>
+            <div className="flex flex-wrap justify-between">
+                <div className="space-x-2 p-6">
+                <h2 className="mb-1 text-xl font-semibold">{title}</h2>
+                <p className="text-sm text-gray-400">{details}</p>
+                </div>
+            </div>
         </div>
     );
 };
